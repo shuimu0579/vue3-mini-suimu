a6acd9be7a3eac46520e60018834549d56fee88d
shuimu0579 <shuimu0579@gmail.com>
2022-04-14 21:32:59 +0800

    feat:44-实现解析 element

--- mini-vue-suimu/src/compiler-core/src/parse.ts
+++ mini-vue-suimu/src/compiler-core/src/parse.ts
@@ -1,5 +1,10 @@
 import { NodeTypes } from "./ast";
 
+const enum TagType {
+  Start,
+  End
+}
+
 export function baseParse(content:string){
   const context = createParserContext(content);
   return createRoot(parseChildren(context))
@@ -8,9 +13,14 @@ export function baseParse(content:string){
 function parseChildren(context){
   const nodes:any = [];
   
+  let s = context.source;
   let node;
-  if(context.source.startsWith("{{")){
+  if(s.startsWith("{{")){
     node = parseInterpolation(context)
+  }else if(s[0] === "<"){
+    if(/[a-z]/i.test(s[1])){
+      node = parseElement(context);
+    }
   }
 
   nodes.push(node);
@@ -55,3 +65,26 @@ function createParserContext(content: string) {
   }
 }
 
+function parseElement(context: any) {
+  // 1.解析tag
+  const element = parseTag(context, TagType.Start);
+  parseTag(context, TagType.End)
+  console.log('------',context.source)
+  return element;
+}
+
+function parseTag(context:any, type:TagType){
+  const match:any = /^<\/?([a-z]*)/i.exec(context.source);
+  // console.log(match);
+  const tag = match[1]
+  // 2.删除处理完成的代码
+  advanceBy(context, match[0].length);
+  console.log(context.source)
+  advanceBy(context, 1);
+  
+  if(type === TagType.End) return;
+  return {
+    type:NodeTypes.ELEMENT,
+    tag
+  }
+}
